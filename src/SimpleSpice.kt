81054a5b2f1b38fe31f7f33cbc1736dab3158d0b
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2026-08-30 21:52:27 -0700

    Agrega la clase SimpleSpice

---
+++ src/SimpleSpice.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val simpleSpice = SimpleSpice()
+    println("${simpleSpice.name} ${simpleSpice.heat}")
+}
+
+class SimpleSpice {
+    val name = "curry"
+    val spiciness = "mild"
+    val heat: Int
+        get() { return 5 }
+}
